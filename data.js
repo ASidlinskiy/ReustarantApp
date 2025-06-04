1861bb1cbfc5e243197b803d6bb1461b5c6cb040
Alex Sidlinskiy <alexsidlinskiy@gmail.com>
2025-06-03 18:38:36 -0700

    added few more items to the menu

--- data.js
+++ data.js
@@ -1,23 +1,86 @@
 const menuArray = [
     {
         name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        ingredients: ["pepperoni", "mushroom", "mozarella"],
         id: 0,
         price: 14,
         emoji: "🍕"
     },
     {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
+        name: "Cheeseburger",
+        ingredients: ["beef patty", "cheddar", "lettuce", "tomato", "onion"],
         price: 12,
         emoji: "🍔",
         id: 1
     },
-        {
+    {
+        name: "Double Bypass Burger",
+        ingredients: ["two beef patties", "bacon", "four cheeses", "special sauce"],
+        price: 18,
+        emoji: "🍔🍔",
+        id: 2
+    },
+    {
+        name: "Veggie Burger",
+        ingredients: ["plant-based patty", "lettuce", "tomato", "pickle", "avocado"],
+        price: 13,
+        emoji: "🥗🍔",
+        id: 3
+    },
+    {
+        name: "Fries",
+        ingredients: ["potatoes", "salt"],
+        price: 4,
+        emoji: "🍟",
+        id: 4
+    },
+    {
+        name: "Onion Rings",
+        ingredients: ["onions", "batter"],
+        price: 5,
+        emoji: "🧅",
+        id: 5
+    },
+    {
+        name: "Cola",
+        ingredients: ["carbonated water", "high fructose corn syrup", "caramel color"],
+        price: 3,
+        emoji: "🥤",
+        id: 6
+    },
+    {
+        name: "Lemonade",
+        ingredients: ["lemons", "sugar", "water"],
+        price: 4,
+        emoji: "🍋",
+        id: 7
+    },
+    {
+        name: "Milkshake",
+        ingredients: ["milk", "ice cream", "flavoring"],
+        price: 6,
+        emoji: "🥛",
+        id: 8
+    },
+    {
+        name: "Coffee",
+        ingredients: ["brewed coffee beans"],
+        price: 4,
+        emoji: "☕",
+        id: 9
+    },
+    {
+        name: "Tea",
+        ingredients: ["tea leaves", "hot water"],
+        price: 3,
+        emoji: "🍵",
+        id: 10
+    },
+    {
         name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
+        ingredients: ["grain", "hops", "yeast", "water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 11 // Adjusted ID due to new additions
     }
-]
+];
